bd72769ec342a1b1bce23b2c2803d684a83553d8
MidoHassan <mi010203269@gmail.com>
2022-03-09 23:53:32 +0200

    app js optmize

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,7 @@
 require('./bootstrap');
 
 window.Vue = require('vue').default;
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { BootstrapVue } from 'bootstrap-vue'
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
@@ -15,8 +15,6 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
 
 import Friends from './components/Friends';
 
